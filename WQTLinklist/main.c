097c7333d75f0932384826b6b89712a6ca9bb0ba
JackyWang2014 <339847016@qq.com>
2016-04-14 11:14:08 +0800

    Create&Add&PrintAll

--- WQTLinklist/main.c
+++ WQTLinklist/main.c
@@ -7,9 +7,81 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct stu{
+    char name[64];
+    
+    int age;
+    int score;
+    struct stu * next;
+}stu_t;
+
+// 创建结点
+stu_t * create_node(void);
+// 添加结点
+void add_node(stu_t * head, stu_t * new_node);
+// 遍历链表
+void traverse_list(stu_t * head);
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+  
+    // 创建链表
+    // 首先创建头结点, 头结点一般不存储数据, 只是作为链表的头部
+    
+    stu_t * head;
+    head = create_node();
+    
+    while (1) {
+        printf("请选择操作:1.添加学生 2.打印学生列表\n");
+        int ctr;
+        scanf("%d",&ctr);
+        if (ctr == 1) {
+            // 创建一个新的结点, 存储学生信息, 添加到链表上
+            stu_t * new_node = create_node();
+            scanf("%s%d%d",new_node->name,&new_node->age,&new_node->score);
+            // 添加到链表上
+            add_node(head, new_node);
+        } else if (ctr == 2) {
+            // 遍历链表, 打印每个结点的信息
+            traverse_list(head);
+        }
+    }
+    
+    
     return 0;
+}
+
+stu_t * create_node(void)
+{
+    stu_t * stu = (stu_t *)malloc(sizeof(stu_t));
+    if (!stu) {
+        perror("malloc");
+        exit(-1);
+    }
+    stu->next = NULL;
+    return stu;
+}
+
+void add_node(stu_t * head, stu_t * new_node)
+{
+    //head->next = new_node;
+    while (head->next) {
+        head = head->next;
+    }
+    // head 指向尾结点
+    head->next = new_node;
+    new_node->next = NULL;
+    
+}
+
+void traverse_list(stu_t * head)
+{
+    // 让指针依次指向每个结点
+    while ((head = head->next)) {
+        printf("姓名: %s 年龄: %d 成绩: %d\n",head->name, head->age, head->score);
+    }
 }
